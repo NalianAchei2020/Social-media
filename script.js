17cb383b654ac8b35f4baeb8494747af2fd9a52a
chiwi2024 <nalianachei2@gmail.com>
2024-10-28 23:34:35 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,14 @@
+const { createBackDatedCommits } = require('./functions');
+
+// Set the desired start date and number of commits
+const startDate = new Date('2024-04-01T00:00:00');
+const numCommits = 200;
+
+// Create the backdated commits
+const commits = createBackDatedCommits(startDate, numCommits);
+
+// Print the list of created commits
+console.log('Created the following backdated commits:');
+commits.forEach((commit, index) => {
+  console.log(`${index + 1}. ${commit}`);
+});
