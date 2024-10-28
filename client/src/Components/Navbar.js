14ec6a67557e18dffcc203dca7e4afc9afd790a5
chiwi2024 <nalianachei2@gmail.com>
2024-10-28 17:20:25 +0100

    add navbar

--- client/src/Components/Navbar.js
+++ client/src/Components/Navbar.js
@@ -1,29 +1,29 @@
 import React, { useEffect, useState } from 'react';
-import './Navbar.css'
+import './Navbar.css';
 
 const Navbar = () => {
-    const [isLogin, setisLogin] = useState(true);
+  const [isLogin, setisLogin] = useState(true);
 
-    useEffect(()=>{
- setisLogin(localStorage.getItem('isLogin'))
-    },[localStorage.getItem('isLogin')])
-    return (
-        <div className="navbar">
-           <a href='/'>Home</a> 
-           <a href='/upload'>Upload</a>
-           {isLogin? (
-            <>
-             <a href='/profile'>Profile</a>
-            </>
-           ):
-           (
-            <>
-            <a href='/register'>Register</a>
-             <a href='/login'>Log In</a>
-            </>
-           )}
-        </div>
-    );
-}
+  useEffect(() => {
+    setisLogin(localStorage.getItem('isLogin'));
+  }, [localStorage.getItem('isLogin')]);
+  return (
+    <div className="navbar">
+      <a href="/">Home</a>
+      <a href="/upload">Upload</a>
+      {isLogin === true ? (
+        <>
+          <a href="/profile">Profile</a>
+          <span>Logout</span>
+        </>
+      ) : (
+        <>
+          <a href="/register">Register</a>
+          <a href="/login">Log In</a>
+        </>
+      )}
+    </div>
+  );
+};
 
 export default Navbar;
