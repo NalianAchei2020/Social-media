1465aa4cd09389423fe3a20200069296e08a5fd7
chiwi2024 <nalianachei2@gmail.com>
2024-10-30 00:54:11 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const { createBackDatedCommits } = require('./functions');
 
 // Set the desired start date and number of commits
-const startDate = new Date('2024-04-01T00:00:00');
+const startDate = new Date('2024-04-02T00:00:00');
 const numCommits = 200;
 
 // Create the backdated commits
